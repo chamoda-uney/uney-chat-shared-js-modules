de7d96c2cf73cee6f9d2ad282f2a8044b8f7710b
varkka <vardan@uney.com>
2024-08-26 16:23:52 +0400

    feat: add ApplicationUserGroup schema, set SCHEMA_VERSION to 12

---
+++ src/realm/schema/application-user-group.ts
@@ -0,0 +1,29 @@
+import Realm, { ObjectSchema } from "realm";
+
+export class ApplicationUserGroup extends Realm.Object<
+  ApplicationUserGroup,
+  "_id" | "_userId" | "_groupId"
+> {
+  _id!: string;
+
+  _userId!: Realm.BSON.ObjectId;
+
+  _groupId!: Realm.BSON.ObjectId;
+
+  static schema: ObjectSchema = {
+    name: "ApplicationUserGroup",
+    primaryKey: "_id",
+
+    properties: {
+      _id: "string",
+      _userId: {
+        type: "objectId",
+        indexed: true,
+      },
+      _groupId: {
+        type: "objectId",
+        indexed: true,
+      },
+    },
+  };
+}
